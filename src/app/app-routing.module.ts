4584c06f0eec8d8b74128c4ae981b34ddc8729e9
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-03-01 08:29:57 -0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [ //the component with an empty path will load first > Home Component
+  { path: '', component: HomeComponent},
+  { path: 'list', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
